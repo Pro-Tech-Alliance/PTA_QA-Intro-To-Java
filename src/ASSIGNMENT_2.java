490b0b63a2534b23a6b0de1a5c7471f6a92c7148
riggsman <57776463+riggsman@users.noreply.github.com>
2022-03-27 20:23:27 +0100

    Add files via upload

---
+++ src/ASSIGNMENT_2.java
@@ -0,0 +1,54 @@
+/*
+ *
+ * AUTHOR EYAMBE RIGOBERT
+ * EMAIL  eyamberigobert2017@gmail.com
+ * DATE   26/03/2022 
+ * 
+ * 
+ * */
+
+package ASSIGNMENT_2;
+
+import java.util.Scanner;
+
+public class ASSIGNMENT_2 {
+
+	 static Scanner sc = new Scanner(System.in);
+	static String StudentName;
+	static String Course = null;
+	static String Grade;
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter student name :: ");
+		StudentName = sc.nextLine();
+		
+		System.out.println("Enter Course :: ");
+		Course = sc.nextLine();	
+		
+		System.out.println("Enter student grade (eg A) :: ");
+		Grade = sc.nextLine();	
+
+	System.out.println(StudentInformation(StudentName,Course) + Grade(Grade));
+		
+
+	}
+	
+
+	 public static String StudentInformation(String _Name, String _Course)
+		{
+		 	
+		 	return  "Student name : "+_Name+"\nCourse : "+_Course;
+			
+		}
+	 
+	 
+	 public static String Grade(String _Grade)
+	 {
+		 
+		 return "\nStudent grade : "+_Grade;
+	 }
+	
+	
+}
