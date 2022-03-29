028eb574025689cfa6dde254093b53a94c8c5e8d
william njie <williamnjie29@gmail.com>
2022-03-29 15:15:06 -0600

    assignment

---
+++ src/NightClub.java
@@ -0,0 +1,65 @@
+
+
+import java.util.Scanner;
+
+/*
+ * Author: Willaim N W
+ * Date  : 03/27/2022
+ * */
+
+public class NightClub {
+	
+	static Scanner Scan = new Scanner(System.in);
+	  
+	static String MaleClubberFirstName,MaleClubberLastName,FemaleClubber2FristName,Femaleclubber2LastName;
+	static int Age, NewAge;
+	static String MaleClubberAddress,FemaleClubberAddress;
+	static double  GateFee1, Gatefee2;
+	static char Gender1, Gender2;
+	static int PaymentMethod;
+	
+	
+	
+	
+	
+	public static void main(String[] args) { 
+		
+		clubberinformation();
+		DeclaringAge();
+	
+	}
+	public static void clubberinformation() {
+	clubberinformation();
+	Scanner Scan = new Scanner(System.in);
+	
+	System.out.println("What is the MaleClubberFirstName ?");
+	MaleClubberFirstName=Scan.nextLine();
+	
+	System.out.println("What is the MaleClubberLastName ?");
+	MaleClubberLastName=Scan.nextLine();
+	
+	System.out.println("What is the FemaleClubber2FristName ?");
+	FemaleClubber2FristName=Scan.nextLine();
+	
+	System.out.println("What is the Femaleclubber2LastName ?");
+	Femaleclubber2LastName=Scan.nextLine();
+	
+	System.out.println("What is MaleClubberAddress ?");
+	MaleClubberAddress=Scan.nextLine();
+	
+	System.out.println("What is FamaleClubberAddress ?");
+	FemaleClubberAddress=Scan.next();
+	
+	}
+	
+	
+	public static void DeclaringAge() 
+	{
+		
+		
+	
+	
+	
+	}
+
+} 
