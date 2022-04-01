273a01a840657a903d13b3e0dbd62476901dc76c
SAPH500 <101662056+SAPH500@users.noreply.github.com>
2022-03-31 23:55:30 -0400

    milo help assignment inheritance

--- src/examples/ClassInstantiation.java
+++ src/examples/ClassInstantiation.java
@@ -3,8 +3,15 @@ package examples;
 public class ClassInstantiation {
 
 	public static void main(String[] args) {
-		
+	
 
+		Example_Concatenation exampleConconcatenationObj = new Example_Concatenation();
+		exampleConconcatenationObj.additionMethod();
+		exampleConconcatenationObj.subtrationMethod();
+		
+		
+		
+		
 	}
 
 }
