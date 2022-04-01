8250625333f9c4cd912bb1b95597e95439528ea8
emileqae <emileesapa@gmail.com>
2022-04-01 17:37:46 -0500

    rework on your project...see corectios

--- src/Instantiation.java
+++ src/Instantiation.java
@@ -1,11 +1,12 @@
 import examples.Example_StringVariable;
 
-public class Instantiation 
+public class Instantiation //Add the opening class brackt here {
 
 	public static void main(String[] args, Object exampleintegerVariable) {
     
 		Example_StringVariable example_integervariableobj = new Example_StringVariable();
-		exampleintegerVariable.age=20;
+		
+		exampleintegerVariable.age=20;//use your Object Name example_integervariableobj not exampleintegerVariable
 		exampleintegerVariable.day=21;
 		
 
