69a0bdfac608ed86666ba7a4b3ab8975485a65e6
emileqae <emileesapa@gmail.com>
2022-04-01 19:40:36 -0500

    assignment April1 assignged

---
+++ src/AssignmentApril1/ExampleFunction.java
@@ -0,0 +1,17 @@
+package AssignmentApril1;
+
+public class ExampleFunction {
+
+	int addTwoNumbers(int val1, int val2) {
+		return val1+val2;
+		
+	}
+	
+	
+	int addThreeNumber(int val1, int val2, int val3) {
+		
+		return val1+val2+val3;
+	}
+	
+	
+}
