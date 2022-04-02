d036341f3750c8355c13470a1840c448bbcd39f0
PROFOBASSE <obaserudolf2@gmail.com>
2022-04-02 16:48:23 -0500

    Initial commit

---
+++ src/March2022/ClassOfallFunctions.java
@@ -0,0 +1,36 @@
+package March2022;
+
+public class ClassOfallFunctions {
+
+	
+public static void main(String[] args) {
+	
+ClassAddSubtMult classOfallFunctionsObj=new	ClassAddSubtMult();
+
+classOfallFunctionsObj.AddingNumberOfGirls(15, 20);
+classOfallFunctionsObj.SubtractNumberOfGirls(20, 10);
+classOfallFunctionsObj.MultiplicationNumberOfGirls(2, 6);
+
+int OutPutA=classOfallFunctionsObj.AddingNumberOfGirls(15, 20);
+int OutPutB=classOfallFunctionsObj.SubtractNumberOfGirls(20, 10);
+int OutPutC=classOfallFunctionsObj.MultiplicationNumberOfGirls(2, 6);
+
+System.out.println(OutPutA);
+System.out.println(OutPutB);
+System.out.println(OutPutC);
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
